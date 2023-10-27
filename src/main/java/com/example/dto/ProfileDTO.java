7ebbf9e9646eeb7dfba03b8b77b941ac3ad9bde8
Asus Tuf <erkinov.0021@gmail.com>
2023-10-27 19:06:35 +0500

    ProfileEntity, ProfileDTO fixed

--- src/main/java/com/example/dto/ProfileDTO.java
+++ src/main/java/com/example/dto/ProfileDTO.java
@@ -1,4 +1,23 @@
 package com.example.dto;
 
+import com.example.enums.ProfileRole;
+import com.example.enums.ProfileStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+@Getter @Setter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfileDTO {
+    private String id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+    private String password;
+    private ProfileStatus status;
+    private ProfileRole role;
+    private Boolean visible;
+    private LocalDateTime createdDate;
+    private String photoId;
 }
--- src/main/java/com/example/entity/ProfileEntity.java
+++ src/main/java/com/example/entity/ProfileEntity.java
@@ -1,4 +1,41 @@
 package com.example.entity;
 
+import com.example.enums.ProfileRole;
+import com.example.enums.ProfileStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@ToString
+@Entity(name = "prfile")
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "profile")
 public class ProfileEntity {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "status")
+    private ProfileStatus status;
+    @Column(name = "role")
+    private ProfileRole role;
+    @Column(name = "visible")
+    private Boolean visible;
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+    @Column(name = "photo_id")
+    private String photoId;
 }
