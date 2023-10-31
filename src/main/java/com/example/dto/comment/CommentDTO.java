2ba1bbe82d93e7c713f5541e9cdc6b035d213a88
Asus Tuf <erkinov.0021@gmail.com>
2023-11-01 01:02:05 +0500

    comment fixed

--- src/main/java/com/example/dto/comment/CommentDTO.java
+++ src/main/java/com/example/dto/comment/CommentDTO.java
@@ -1,4 +1,15 @@
 package com.example.dto.comment;
 
+import jakarta.persistence.Column;
+import lombok.*;
+
+@Getter @Setter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentDTO {
+    private String profileId;
+    private String content;
+    private Integer articleId;
+    private Integer replyId;
+    private Boolean visible;
 }
--- src/main/java/com/example/entity/comment/CommentEntity.java
+++ src/main/java/com/example/entity/comment/CommentEntity.java
@@ -1,5 +1,31 @@
 package com.example.entity.comment;
 
-public class CommentEntity {
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+@Getter @Setter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "comment")
+@Table(name = "comment")
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+    @Column(name = "profile_id")
+    private String profileId;
+    @Column(name = "content")
+    private String content;
+    @Column(name = "article_id")
+    private Integer articleId;
+    @Column(name = "reply_id")
+    private Integer replyId;
+    @Column(name = "visible")
+    private Boolean visible;
 }
